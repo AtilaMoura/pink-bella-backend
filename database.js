8993ce678751d4056db4fcb496fc5eeac4d73509
AtilaMoura <atilagmoura@gmail.com>
2025-06-17 10:09:46 -0300

    feat: suporte a múltiplos ambientes (.env) com seleção dinâmica do banco

--- database.js
+++ database.js
@@ -1,8 +1,9 @@
 const sqlite3 = require('sqlite3').verbose();
 const path = require('path');
+require('dotenv').config();
 
 // Define o caminho para o arquivo do banco de dados
-const dbPath = path.resolve(__dirname, 'database.sqlite');
+const dbPath = path.resolve(__dirname, process.env.DB_PATH);
 const db = new sqlite3.Database(dbPath, (err) => {
     if (err) {
         console.error('Erro ao abrir o banco de dados:', err.message);
